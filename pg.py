ff5ce4880dde3b4b1dbe2515ec40867dc6e131ac
Kai Ke <ken19505@gmail.com>
2024-10-26 19:39:21 +0800

    now get the road and river data

--- pg.py
+++ pg.py
@@ -6,7 +6,7 @@ from rasterio.features import geometry_mask, rasterize
 from rasterio.transform import from_bounds
 from shapely.geometry import box
 import matplotlib.pyplot as plt
-from skimage.segmentation import find_boundaries # to draw only the border
+from skimage.segmentation import find_boundaries  # to draw only the border
 
 FMM_REP_DATA_PATH = "replicate_fmm/Data/china/"
 
@@ -36,9 +36,10 @@ Potential Road data
    https://rarebooks-maps.npm.edu.tw/index.php?act=Archive/drawing/1-20/eyJzZWFyY2giOltdLCJ0cGZpbHRlciI6eyJwcV9zdWJqZWN0Ijp7InRlcm1zIjpbIui8v%2BWcsOmhniJdfX19   
 '''
 
+
 def peep_geo(map_name):
     cur_map = gpd.read_file(
-        f"{FMM_REP_DATA_PATH}CHGis/{map_name}/", 
+        f"{FMM_REP_DATA_PATH}CHGis/{map_name}/",
         encoding='utf-8'
     )
 
@@ -48,9 +49,15 @@ def peep_geo(map_name):
     print(cur_map.head(5))
 
     # print(cur_map[['SYS_ID', 'NAME_FT']])
+    print(cur_map[['SEA_ID', 'SOURCE', 'ARC_ID']])
+
+    # Get distribution of "type"
+    # check_dist = cur_map['type'].value_counts()
+    # print(check_dist)
 
     # check = cur_map.head(5)
     # check.to_csv(f"{FMM_REP_DATA_PATH}{mn}.csv", index=False)
+
 
 def contour_matrix(map_name: str) -> np.ndarray:
     '''
@@ -61,7 +68,7 @@ def contour_matrix(map_name: str) -> np.ndarray:
     2d ndarray
     '''
     cur_map = gpd.read_file(
-        f"{FMM_REP_DATA_PATH}CHGis/{map_name}/", 
+        f"{FMM_REP_DATA_PATH}CHGis/{map_name}/",
         encoding='utf-8'
     )
 
@@ -83,15 +90,16 @@ def contour_matrix(map_name: str) -> np.ndarray:
 
     # Rasterize the polygons
     transform = rasterio.transform.from_bounds(
-        minx, miny, maxx, maxy, 
+        minx, miny, maxx, maxy,
         width=cols, height=rows
     )
 
     # Iterate through each polygon in the GeoDataFrame and update the matrix
     for geom in cur_map['geometry']:
         # Convert the polygon to a rasterized mask
-        mask = geometry_mask([geom], transform=transform, invert=True, out_shape=(rows, cols))
-        
+        mask = geometry_mask([geom], transform=transform,
+                             invert=True, out_shape=(rows, cols))
+
         # Update the matrix where the mask is True
         matrix[mask] = 1
 
@@ -101,17 +109,14 @@ def contour_matrix(map_name: str) -> np.ndarray:
         'maxx': maxx,
         'maxy': maxy,
         'rows': rows,
-        'cols': cols, # would like to check if transform carries the data above, if yes then no need to keep
+        'cols': cols,  # would like to check if transform carries the data above, if yes then no need to keep
         'transform': from_bounds(minx, miny, maxx, maxy, cols, rows),
         'crs': cur_map.crs
     }
 
+    print('Contour Done.')
     return matrix, map_info
 
-def road_matrix(filename: str):
-    road_data = gpd.read_file(f"{FMM_REP_DATA_PATH}{filename}")
-
-    print(road_data.head())
 
 def line_to_matrix(filename: str, map_info: dict):
     '''
@@ -119,28 +124,50 @@ def line_to_matrix(filename: str, map_info: dict):
         'FNODE_', 'TNODE_', 'LENGTH', 'CODED_RIVE', 'CODED_RI_1', 'KEY_ID',
         'SYSTEM_ID', 'MOUTH', 'ORDER_', 'RIVERS_ID', 'KEY_ID_1', 'NAME_CH',
         'NAME_FT', 'NAME_PY', 'MIDPOINT_X', 'MIDPOINT_Y', 'geometry'
-    
-    '''
-    cur_lines = gpd.read_file(
-        f"{FMM_REP_DATA_PATH}CHGis/{filename}/", 
-        encoding='utf-8'
-    )
 
-    # Define the transform (maps pixel coordinates to geographic coordinates)
-    transform = map_info['transform']
+    Coast line:
+        'SEA_ID', 'SOURCE', 'ARC_ID', 'geometry'
+
+    Ming roads:
+        columns: 'type', 'name', 'geometry'
+        type: 陸(1009) or 水(436) or 水陸(23)
+        name: start ~ end, eg. 會同館～固節驛
+
+    '''
+    value_mapping = {'陸': 1, '水': 0.5, '水陸': 0.2}
+
+    if filename == 'ming_traffic':
+        cur_lines = gpd.read_file(
+            f"{FMM_REP_DATA_PATH}{filename}/",
+            encoding='utf-8'
+        )
+        # the original LINESTRING currently is in normal long-lat format
+        cur_lines = cur_lines.set_crs("EPSG:4326")
+        cur_lines = cur_lines.to_crs(map_info['crs'])
+    else:
+        cur_lines = gpd.read_file(
+            f"{FMM_REP_DATA_PATH}CHGis/{filename}/",
+            encoding='utf-8'
+        )
+
+    if not 'type' in cur_lines.columns:
+        cur_lines['type'] = '陸'
 
     # Rasterize the river geometries
     raster = rasterize(
-        ((geom, 1) for geom in cur_lines['geometry']),
+        ((geom, value_mapping[road_type]) for geom, road_type in zip(
+            cur_lines['geometry'], cur_lines['type'])),
         out_shape=(map_info['rows'], map_info['cols']),
-        transform=transform,
+        # maps pixel coordinates to geographic coordinates
+        transform=map_info['transform'],
         fill=0,  # Value for empty cells
-        all_touched=True,  # Include all pixels touched by river
+        all_touched=True,  # Include all pixels touched by river, coast line, roads
     )
 
     return raster
 
-def plot_matrix(mat: np.ndarray, map_info: dict, title: str = "some title", save_fig=True):
+
+def plot_matrix(mat: np.ndarray, title: str = "some title", save_fig=True):
     n, m = mat.shape
     fig, ax = plt.subplots()
     cax = ax.imshow(mat, cmap='gray', interpolation='none')
@@ -149,48 +176,39 @@ def plot_matrix(mat: np.ndarray, map_info: dict, title: str = "some title", save
 
     if save_fig:
         plt.savefig(
-            f'{FMM_REP_DATA_PATH}raw/{title}.tif', 
+            f'{FMM_REP_DATA_PATH}raw/{title}.tif',
             format='tiff',
             dpi=600
         )
-        # with rasterio.open(
-        #     f'{FMM_REP_DATA_PATH}raw/{title}.tif', 'w',
-        #     driver='GTiff',
-        #     height=n,
-        #     width=m,
-        #     count=1,  # Single band
-        #     dtype=rasterio.uint8,
-        #     crs=map_info['crs'],  # WGS84 coordinate system (latitude/longitude)
-        #     transform=map_info['transform'],  # Affine transform (links pixels to geographic coords)
-        # ) as dst:
-        #     dst.write(mat, 1)
     else:
         plt.show()
 
+
 def main():
     # lin = LINESTRING; pgn = POLYGON
-    
+
     mn = "v5_1820_coast_lin"
     # peep_geo(mn)
 
     # contour
-    cont = "v6_1820_prov_pgn_utf" # NAME_FT is traditional chinese
-    cont_mat, map_info = contour_matrix(cont) # 4761, 6075
-    # plot_matrix(cont_mat, map_info, title='China - outline', save_fig=False)
+    cont = "v6_1820_prov_pgn_utf"  # NAME_FT is traditional chinese
+    cont_mat, map_info = contour_matrix(cont)  # 4761, 6075
+    # plot_matrix(cont_mat, title='China - outline', save_fig=False)
 
     # river
     rvr = "v6_1820_coded_rvr_lin_utf"
-    rvr_mat = line_to_matrix(rvr, map_info=map_info)
-    plot_matrix(rvr_mat, map_info, title='China - rivers', save_fig=False)
+    # rvr_mat = line_to_matrix(rvr, map_info=map_info)
+    # plot_matrix(rvr_mat, title='China - rivers', save_fig=False)
 
     # coast
     coast = "v5_1820_coast_lin"
     # coast_mat = line_to_matrix(coast, map_info=map_info)
-    # plot_matrix(coast_mat, map_info, title='China - coastline', save_fig=False)
+    # plot_matrix(coast_mat, title='China - coastline', save_fig=False)
 
     # road data
-    road_ming = 'ming_post_station_road.kml'
-    # road_matrix(road_ming)
+    road_ming = 'ming_traffic'
+    # road_mat = line_to_matrix(road_ming, map_info=map_info)
+    # plot_matrix(road_mat, title='China - Ming Roads', save_fig=False)
 
     print("Done")
 
