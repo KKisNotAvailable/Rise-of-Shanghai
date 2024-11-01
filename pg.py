aa142dda78e008392edb52c13ad1529954e93e72
KYK <ken19505@gmail.com>
2024-11-01 18:17:10 +0800

    can draw sea now, but there are problems mapping real world long and lat onto the drawn map.

--- pg.py
+++ pg.py
@@ -6,9 +6,13 @@ from rasterio.features import geometry_mask, rasterize
 from rasterio.transform import from_bounds
 from shapely.geometry import box
 import matplotlib.pyplot as plt
+from pyproj import Transformer
+from typing import List
 from skimage.segmentation import find_boundaries  # to draw only the border
 
 FMM_REP_DATA_PATH = "replicate_fmm/Data/china/"
+EXTRA_MAP_PATH = "replicate_fmm/Data/ne_10m_admin_0_countries/" # for the sea information near china
+NORMAL_LON_LAT_CRS = 'EPSG:4326'
 
 '''NOTE
 建議可以查XX省水路輿圖或數位方輿裡面查"省輿圖"
@@ -59,6 +63,26 @@ def peep_geo(map_name):
     # check.to_csv(f"{FMM_REP_DATA_PATH}{mn}.csv", index=False)
 
 
+def polygon_to_matrix(cur_map, map_info):
+    '''
+    This function is to rasterize maps with polygon as geometry.
+    Used on China outline and sea.
+    '''
+    rows, cols = map_info['rows'], map_info['cols']
+    matrix = np.zeros((rows, cols), dtype=int)
+
+    # Iterate through each polygon in the GeoDataFrame and update the matrix
+    for geom in cur_map['geometry']:
+        # Convert the polygon to a rasterized mask
+        mask = geometry_mask([geom], transform=map_info['transform'],
+                             invert=True, out_shape=(rows, cols))
+
+        # Update the matrix where the mask is True
+        matrix[mask] = 1
+
+    return matrix
+
+
 def contour_matrix(map_name: str) -> np.ndarray:
     '''
     transform the border / land area to matrix and plot
@@ -75,44 +99,27 @@ def contour_matrix(map_name: str) -> np.ndarray:
     # 25047~25051 是南沙群島 (萬里長沙, 千里石塘, 曾母暗沙, 東沙, 中沙)
     cur_map = cur_map[cur_map['SYS_ID'] <= '25047']
 
-    # print(cur_map['NAME_FT'])
+    minx, miny, maxx, maxy = cur_map.total_bounds
+
+    # for correctly set the boundries for other matrices
+    normal_lon_lat_map = cur_map.to_crs(NORMAL_LON_LAT_CRS)
 
     adjust_scale = 1000
-
-    minx, miny, maxx, maxy = cur_map.total_bounds
 
     # determine the size of matrix
     cols = int((maxx - minx) / adjust_scale)
     rows = int((maxy - miny) / adjust_scale)
 
-    # Create a numpy matrix to hold the rasterized data
-    matrix = np.zeros((rows, cols), dtype=int)
-
-    # Rasterize the polygons
-    transform = rasterio.transform.from_bounds(
-        minx, miny, maxx, maxy,
-        width=cols, height=rows
-    )
-
-    # Iterate through each polygon in the GeoDataFrame and update the matrix
-    for geom in cur_map['geometry']:
-        # Convert the polygon to a rasterized mask
-        mask = geometry_mask([geom], transform=transform,
-                             invert=True, out_shape=(rows, cols))
-
-        # Update the matrix where the mask is True
-        matrix[mask] = 1
-
     map_info = {
-        'minx': minx,
-        'miny': miny,
-        'maxx': maxx,
-        'maxy': maxy,
+        'lon_lat_bounds': normal_lon_lat_map.total_bounds,
         'rows': rows,
         'cols': cols,  # would like to check if transform carries the data above, if yes then no need to keep
         'transform': from_bounds(minx, miny, maxx, maxy, cols, rows),
         'crs': cur_map.crs
     }
+
+    # Rasterize the polygons
+    matrix = polygon_to_matrix(cur_map=cur_map, map_info=map_info)
 
     print('Contour Done.')
     return matrix, map_info
@@ -142,7 +149,7 @@ def line_to_matrix(filename: str, map_info: dict):
             encoding='utf-8'
         )
         # the original LINESTRING currently is in normal long-lat format
-        cur_lines = cur_lines.set_crs("EPSG:4326")
+        cur_lines = cur_lines.set_crs(NORMAL_LON_LAT_CRS)
         cur_lines = cur_lines.to_crs(map_info['crs'])
     else:
         cur_lines = gpd.read_file(
@@ -167,6 +174,82 @@ def line_to_matrix(filename: str, map_info: dict):
     return raster
 
 
+def sea_matrix(map_info: dict, map_path: str = EXTRA_MAP_PATH):
+    '''
+    Since the outline of China does not indicate where the sea locates,
+    I thus use the East Asian countries outline to determine.
+    The sea matrix will serve as the 'road' where people can sail.
+    '''
+    world = gpd.read_file(map_path)
+
+    lon_min, lat_min, lon_max, lat_max = map_info['lon_lat_bounds']
+
+    east_asia = gpd.clip(world, box(lon_min, lat_min, lon_max, lat_max))
+    east_asia = east_asia.to_crs(map_info['crs'])
+
+    # Rasterize the polygons
+    matrix = polygon_to_matrix(cur_map=east_asia, map_info=map_info)
+
+    # This matrix has lands as 1 and sea as 0, we shall flip them
+    matrix = abs(matrix - 1)
+    
+    # There are no sea available for China on the left (left of long 105 E)
+    # we'll set those pixals to 0 as well ('land')
+    # NOTE: even if we don't care about the lat, it still need to be in the 
+    #       range of original map, otherwise the transform would return some
+    #       wierd number 
+    bound_lon, bound_lat = 105, 30 
+
+    row_idx, col_idx = lonlat_to_idx(point=[bound_lon, bound_lat], map_info=map_info)
+
+    print(map_info['rows'], map_info['cols'])
+    print(row_idx, col_idx)
+
+    # matrix[:, :col_idx] = 0
+
+    return matrix
+
+
+def lonlat_to_idx(point, map_info):
+    '''
+    This function would use the transform from the map_info to
+    convert the desired point to the pixel index on the matrix
+    '''
+    lon, lat = point
+    transformer = Transformer.from_crs(NORMAL_LON_LAT_CRS, map_info['crs'], always_xy=True)
+    lon, lat = transformer.transform(lon, lat)
+
+    row_idx, col_idx = ~map_info['transform'] * (lon, lat)
+
+    return int(np.floor(row_idx)), int(np.floor(col_idx))
+
+
+def add_points_on_map(
+    target_map,
+    map_info,
+    points: List[List[int]] = [[121.564558, 25.03746]],
+    radius: int = 10,
+    value: int = 2
+):
+    rows, cols = target_map.shape
+
+    for p in points:
+        # TODO: maybe should check if the point actually need transformation
+        row_c, col_c = lonlat_to_idx(p, map_info)
+        # Define the bounding box around the center
+        row_min = max(row_c - radius, 0)
+        row_max = min(row_c + radius + 1, rows)
+        col_min = max(col_c - radius, 0)
+        col_max = min(col_c + radius + 1, cols)
+
+        # Iterate over only the bounding box
+        for row in range(row_min, row_max):
+            for col in range(col_min, col_max):
+                if (row - row_c) ** 2 + (col - col_c) ** 2 <= radius ** 2:
+                    target_map[row, col] = value
+    
+
+
 def plot_matrix(mat: np.ndarray, title: str = "some title", save_fig=True):
     n, m = mat.shape
     fig, ax = plt.subplots()
@@ -186,29 +269,44 @@ def plot_matrix(mat: np.ndarray, title: str = "some title", save_fig=True):
 
 def main():
     # lin = LINESTRING; pgn = POLYGON
+    # bounds: [ 69.75457966  15.78138    144.75150107  55.92380503]
+    # transform:
+    #   | 1000.02, 0.00, 15923439.01|
+    #   | 0.00,-1000.03, 6506625.71|
+    #   | 0.00, 0.00, 1.00|
 
     mn = "v5_1820_coast_lin"
     # peep_geo(mn)
 
+    # NOTE: For all the following matices, I assume them to including only 0 and 1
     # contour
     cont = "v6_1820_prov_pgn_utf"  # NAME_FT is traditional chinese
     cont_mat, map_info = contour_matrix(cont)  # 4761, 6075
-    # plot_matrix(cont_mat, title='China - outline', save_fig=False)
+    add_points_on_map(cont_mat, map_info)
+    plot_matrix(cont_mat, title='China - outline', save_fig=False)
 
     # river
     rvr = "v6_1820_coded_rvr_lin_utf"
     # rvr_mat = line_to_matrix(rvr, map_info=map_info)
     # plot_matrix(rvr_mat, title='China - rivers', save_fig=False)
 
+    # road data
+    road_ming = 'ming_traffic'
+    # road_mat = line_to_matrix(road_ming, map_info=map_info)
+    # plot_matrix(road_mat, title='China - Ming Roads', save_fig=False)
+
+    # NOTE: I'm thinking do we need coast anymore? since there's no reason to 
+    #       limit sailing only along the coast, should there be a huge bay but 
+    #       the destination need to ignore the bay and sail straightly,
+    #       in such case, limiting to coast line would overestimate the time
     # coast
     coast = "v5_1820_coast_lin"
     # coast_mat = line_to_matrix(coast, map_info=map_info)
     # plot_matrix(coast_mat, title='China - coastline', save_fig=False)
 
-    # road data
-    road_ming = 'ming_traffic'
-    # road_mat = line_to_matrix(road_ming, map_info=map_info)
-    # plot_matrix(road_mat, title='China - Ming Roads', save_fig=False)
+    # sea data
+    # sea_mat = sea_matrix(map_info=map_info)
+    # plot_matrix(sea_mat, title='China - sea nearby', save_fig=False)
 
     print("Done")
 
